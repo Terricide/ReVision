946b36d79b110632d1a64673b1e7b06d2b4215a4
Mark Reed <mreed@verismic.com>
2016-04-13 01:47:53 -0600

    pulled qooxdoo into own solution

--- src/Bridge.Qooxdoo/qx/ui/form/Button.cs
+++ src/Bridge.Qooxdoo/qx/ui/form/Button.cs
@@ -3,7 +3,6 @@ using Bridge.Html5;
 using qx.ui.basic;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
